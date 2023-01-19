556f03e2c5b9ce32193af82e0cd37299517cf22a
psjuliano <psouzajuliano@hotmail.com>
2023-01-19 21:45:52 +0000

    directives component update

--- curso-angular13/src/app/app.module.ts
+++ curso-angular13/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { PrimeiroComponenteComponent } from './components/primeiro-componente/primeiro-componente.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
-    PrimeiroComponenteComponent
+    PrimeiroComponenteComponent,
+    DirectivesComponent
   ],
   imports: [
     BrowserModule,
