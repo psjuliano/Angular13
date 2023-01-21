a8c4a77bff68247c97824a28caa853e4767db7d3
psjuliano <psouzajuliano@hotmail.com>
2023-01-21 16:35:20 +0000

    services update

--- curso-angular13/src/app/services/list.service.ts
+++ curso-angular13/src/app/services/list.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Animal } from '../Animal';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,10 @@ import { Injectable } from '@angular/core';
 export class ListService {
 
   constructor() { }
+
+  remove(animals: Animal[], animal: Animal){
+    //console.log("Ativando services")
+    return animals.filter((a) => animal.name !== a.name);
+  }
+
 }
